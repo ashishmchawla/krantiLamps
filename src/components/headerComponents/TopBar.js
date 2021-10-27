15989cedda6f01580f0304eae5caa47feed8c7e5
Ashish Chawla <ashishmchawla@gmail.com>
2021-10-28 00:59:07 +0530

    Adding top bar and icons structure

---
+++ src/components/headerComponents/TopBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const TopBar = () => {
+    return (
+        <Container className="top-bar">
+            <Row></Row>
+        </Container>
+    );
+};
+
+export default TopBar;
