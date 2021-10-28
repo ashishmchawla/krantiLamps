726821b38bfc47f9c0d214ec7f2808ac5cb9557f
Ashish Chawla <ashishmchawla@gmail.com>
2021-10-28 23:42:29 +0530

    Setting Up Redux

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./theme/scss/main.scss";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </React.StrictMode>
+    </Provider>,
     document.getElementById("root")
 );
 
