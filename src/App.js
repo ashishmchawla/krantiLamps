a83bd3b337de6d89d9597bdc8aa72ebbeb5bf378
Ashish Chawla <ashishmchawla@gmail.com>
2021-10-28 10:06:16 +0530

    Adding Top Bar & Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./theme/scss/main.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import TopBar from "./components/headerComponents/TopBar";
+import Navigation from "./components/headerComponents/Navigation";
 
 function App() {
     return (
         <div className="App">
             <TopBar />
+            <Navigation />
         </div>
     );
 }
--- src/components/headerComponents/TopBar.js
+++ src/components/headerComponents/TopBar.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { FaPhoneAlt } from "react-icons/fa";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 const TopBar = () => {
     return (
-        <Container className="top-bar">
+        <Container className="top-bar" fluid>
             <Row>
-                <Col></Col>
-                <Col></Col>
-                <Col>
-                    <div className="top-bar__inline_items">
-                        <FaPhoneAlt />
-                        0251-2580074
+                <Col lg={{ span: 4, offset: 8 }} sm={{ span: 12 }}>
+                    <div className="top-bar__items">
+                        <a className="top-bar__item" href="tel:0251 2580074">
+                            <FaPhoneAlt className="top-bar__item__icon" />
+                            0251-2580074
+                        </a>
+                        <a
+                            className="top-bar__item"
+                            href="mailto:info@krantilamps.com"
+                        >
+                            <FaEnvelope className="top-bar__item__icon" />
+                            info@krantilamps.com
+                        </a>
+                        <a className="top-bar__item" href="google.com">
+                            <FaMapMarkerAlt className="top-bar__item__icon" />
+                            Kalyan
+                        </a>
                     </div>
                 </Col>
             </Row>
