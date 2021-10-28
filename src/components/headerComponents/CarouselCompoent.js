da8f85725dca9f7ec53f28bb5e918d61abacb872
Ashish Chawla <ashishmchawla@gmail.com>
2021-10-28 11:16:04 +0530

    Carousel Styling

--- src/components/headerComponents/CarouselCompoent.js
+++ src/components/headerComponents/CarouselCompoent.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import firstImage from "../../theme/images/1.png";
+import secondImage from "../../theme/images/2.png";
+import thirdImage from "../../theme/images/3.png";
 
 const CarouselCompoent = () => {
     return (
@@ -8,44 +11,43 @@ const CarouselCompoent = () => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=373940"
+                        src={firstImage}
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
+                        <h3>Welcome to Kranti Lamps</h3>
                         <p>
-                            Nulla vitae elit libero, a pharetra augue mollis
-                            interdum.
+                            We are one of the leading Manufacturers of LED
+                            Lamps.
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        src={secondImage}
                         alt="Second slide"
                     />
-
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
+                        <h3>Welcome to Kranti Lamps</h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit.
+                            With Product range from 0.5 watt Decorative Lamps to
+                            200 watt Outdoor Luminaries.
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        src={thirdImage}
                         alt="Third slide"
                     />
-
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
+                        <h3>Welcome to Kranti Lamps</h3>
                         <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl
-                            consectetur.
+                            Our key focus and relentless effort has been to
+                            deliver Products with utmost innovation and Quality.
+                            This is what sets us apart from the competitors.
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
--- src/components/headerComponents/Navigation.js
+++ src/components/headerComponents/Navigation.js
@@ -1,34 +1,29 @@
 import React from "react";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav, Image } from "react-bootstrap";
+import logo from "../../theme/images/logo.png";
 
 const Navigation = () => {
     return (
         <Navbar sticky="top" bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <Image
+                        src={logo}
+                        alt="Kranti Lamps Logo"
+                        fluid
+                        style={{ maxWidth: "120px" }}
+                    />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse
                     id="basic-navbar-nav"
                     className="justify-content-end"
                 >
                     <Nav>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">
-                                Action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">
-                                Something
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/products">Products</Nav.Link>
+                        <Nav.Link href="/download">Download</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
